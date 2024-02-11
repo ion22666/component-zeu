1a4a4a8e2239820d60222c4d7709017a6411db59
ion2266 <ionmocanu2266@gmail.com>
2024-02-11 15:53:04 +0200

    created first project structure

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-import "./lib";
+import { components } from "./components";
+import "./styles/global.scss";
 
-console.log("Hello World" as string);
+components.forEach((C) => document.createElement(new C().nodeName));
+
+window.onload = () => {
+	components.forEach((C) => {
+		document.querySelectorAll<HTMLUnknownElement>(new C().nodeName).forEach((el) => new C().mount(el));
+	});
+};
